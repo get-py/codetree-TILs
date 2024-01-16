84989b9f8c98a9e71d6abd689d608271baac235e
YERIN PARK <93588135+get-py@users.noreply.github.com>
2024-01-16 09:16:33 +0900

    Title: 체질량지수, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240116/체질량지수/bmi.py
+++ 240116/체질량지수/bmi.py
@@ -1,9 +1,10 @@
 inp = input()
 arr = inp.split()
-height = int(arr[0]) 
+height = int(arr[0]) / 100
 weight = int(arr[1])
-bmi = weight * 100 * 100 // (height * height)
 
-print(bmi)
+bmi = weight // (height * height)
+
+print(int(bmi))
 if bmi >= 25:
     print('Obesity')

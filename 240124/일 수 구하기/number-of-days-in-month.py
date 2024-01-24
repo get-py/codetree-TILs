c780a582268fea5f60cbac9a6bc1567e788aaa19
YERIN PARK <93588135+get-py@users.noreply.github.com>
2024-01-24 09:28:42 +0900

    Title: 일 수 구하기, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240124/일 수 구하기/number-of-days-in-month.py
+++ 240124/일 수 구하기/number-of-days-in-month.py
@@ -2,7 +2,7 @@ n = int(input())
 
 if n == 2:
     print(28)
-elif n == 4 or 6 or 9 or 11:
+elif (n == 4) or (n == 6) or (n == 9) or (n == 11):
     print(30)
-else:
+elif n == 1 or 3 or 5 or 7 or 8 or 10 or 12:
     print(31)
